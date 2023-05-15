eed846d4eb457780f335f6c74ef0ff16342c584c
Maksim Semenov <maksimuma@list.ru>
2023-05-15 21:09:17 +0300

    hotfix: настроить env файл

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -9,8 +9,8 @@ const AuthError = require('../errors/AuthError');
 const ConflictError = require('../errors/ConflictError');
 
 const {
-  NODE_ENV = 'production',
-  JWT_SECRET = 'eb28135ebcfc17578f96d4d65b6c7871f2c803be4180c165061d5c2db621c51b',
+  NODE_ENV,
+  JWT_SECRET,
 } = process.env;
 
 const getUsers = (req, res, next) => {
