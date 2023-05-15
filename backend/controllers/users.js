1b2315e13c2c82193406a5f4e22a3b66d24395a7
Maksim Semenov <maksimuma@list.ru>
2023-05-15 20:03:59 +0300

    hotfix: тест контроллеров

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -9,8 +9,8 @@ const AuthError = require('../errors/AuthError');
 const ConflictError = require('../errors/ConflictError');
 
 const {
-  NODE_ENV,
-  JWT_SECRET,
+  NODE_ENV = 'production',
+  JWT_SECRET = 'eb28135ebcfc17578f96d4d65b6c7871f2c803be4180c165061d5c2db621c51b',
 } = process.env;
 
 const getUsers = (req, res, next) => {
@@ -127,13 +127,7 @@ const updateAvatar = (req, res, next) => {
       runValidators: true,
     },
   ).orFail(new NotFoundError())
-    .then((user) => res.send({
-      _id: user._id,
-      name: user.name,
-      about: user.about,
-      avatar: user.avatar,
-      email: user.email,
-    }))
+    .then((user) => res.send({ data: user }))
     .catch((err) => {
       if (err instanceof mongoose.Error.NotFoundError) {
         next(new NotFoundError('Пользователя с таким ID нет'));
